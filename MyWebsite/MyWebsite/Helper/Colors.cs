12ca263a6b0e6a456c18947f02264b518efb9a7b
mo <sizdah@gmail.com>
2021-04-11 09:59:07 -0400

    Initial commit

---
+++ MyWebsite/MyWebsite/Helper/Colors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebsite.Helper
+{
+    public class Colors
+    {
+        public static string selected = "color:#fd7e14;";
+    }
+}
